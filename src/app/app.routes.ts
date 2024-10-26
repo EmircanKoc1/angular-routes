15aa3d3b2bc646a1f995a2e264037fb46c26c8b3
Emircan <emircandv787@gmail.com>
2024-10-27 01:26:11 +0300

    New components have been added and route has been configured.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { Route, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "/home",
+        pathMatch: "full"
+    }
+    , {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: "contact",
+        component: ContactComponent
+    },
+    {
+        path: "**",
+        component: NotfoundComponent
+    }
+];
