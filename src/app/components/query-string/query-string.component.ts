d0449c14aababbbcc9d1763780143967297ec9a9
Emircan <emircandv787@gmail.com>
2024-10-27 22:29:56 +0300

    The querystring component has been created.

---
+++ src/app/components/query-string/query-string.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-query-string',
+  standalone: true,
+  imports: [],
+  templateUrl: './query-string.component.html',
+  styleUrl: './query-string.component.scss'
+})
+export class QueryStringComponent {
+
+}
